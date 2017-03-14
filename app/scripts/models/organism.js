6454a70debc608783f18660574b378eadca39285
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-03-14 11:41:18 -0400

    bug fix

--- app/scripts/models/organism.js
+++ app/scripts/models/organism.js
@@ -7,7 +7,7 @@ var Backbone = require('backbone');
 //Models
 //********************************
 var Organism = Backbone.Model.extend({
-  
+
 });
 
 //********************************
@@ -15,12 +15,13 @@ var Organism = Backbone.Model.extend({
 //********************************
 //url function to search for search term on API
 var OrganismCollection = Backbone.Collection.extend({
-model: Organism,
-url: function() {
-  var baseURL = 'https://api.gbif.org/v1/species/search?q=';
-  var endURL = '&dataset_key=d7dddbf4-2cf0-4f39-9b2a-bb099caae36c&rank=SPECIES';
-  var url = baseURL + this.searchTerm + endURL;
-  return url;
+  model: Organism,
+  url: function() {
+    var baseURL = 'https://api.gbif.org/v1/species/search?q=';
+    var endURL = '&dataset_key=d7dddbf4-2cf0-4f39-9b2a-bb099caae36c&rank=SPECIES';
+    var url = baseURL + this.searchTerm + endURL;
+
+    return url;
 
   }
 });
